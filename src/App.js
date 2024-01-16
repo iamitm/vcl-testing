8a14f192edaa08eebf346f58669a5e1958188896
Amit Mishra <amit.mish98@gmail.com>
2024-01-16 14:53:44 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import FPComponent from './FPComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+        Hello World!
+      </p>
+      <FPComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/FPComponent.js
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import {useEffect} from 'react';
+import * as FingerprintJS from '@fingerprintjs/fingerprintjs-pro';
+
+export default function FPComponent() {
+    useEffect(() => {
+        async function fetchFP() {
+            const fpLib = await FingerprintJS.load({
+                apiKey: "mRqCZQen4dS2bsC65Ncv",
+                endpoint: [
+                    'https://vcltesting.xyz/fingerprint/vendor/identify/',
+                ],
+                scriptUrlPattern: [
+                    'https://vcltesting.xyz/fingerprint/vendor/sdk/v<version>/<apiKey>/loader_v<loaderVersion>.js'
+                ]
+            });
+        
+            const fpRes = await fpLib.get();
+            console.log(fpRes);
+        }
+        fetchFP();
+    }, []);
+
+    return <></>;
+}
